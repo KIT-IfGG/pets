5b8585f16f2e9c1d5247cd5264a63d3f393d8924
kdolos <dolos@kit.edu>
2017-10-09 13:53:39 +0200

    del

--- vignettes/beetles.R
+++
@@ -1,5 +0,0 @@
-## ----Simulate, eval=TRUE, echo=TRUE, include=TRUE, results='hide', fig.keep='last', fig.show='asis', strip.white=TRUE, fig.align='center', fig.width=7, fig.height=4----
-#library(pets)
-#beetles_simulate(n_steps=10, world_size=100, dispPar=20, plot_flag=TRUE, col_forest=c("brown", "green", "darkgreen"), col_beetle=c("white", "blue"),  regrowth=1, sleep=0.3, K=10, young_forest=3, beetle_reproduction=0.7, write_out = TRUE)
-
-
