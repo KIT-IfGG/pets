b81c739e7a874688e285497b5688e8808e9a40ec
kdolos <dolos@kit.edu>
2017-10-25 09:33:44 +0200

    vigentts bugs...

---
+++ vignettes/neutral_cummunity_model.R
@@ -0,0 +1,5 @@
+## ----Simulate, eval=TRUE, echo=TRUE, include=TRUE, results='hide', fig.keep='last', fig.show='asis', strip.white=TRUE, fig.align='center', fig.width=7, fig.height=4----
+library(pets)
+neutral_community_model(nspecies=8, gridsize=20, time=100, pdf=FALSE, plotpop=TRUE, sleep=0)
+
+
